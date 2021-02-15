0b77677181f9709b7f0b73a733b531b3888f3057
kousu0530 <72140002+kousu0530@users.noreply.github.com>
2021-02-15 18:01:42 +0900

    修正７

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -78,8 +78,11 @@ class ItemsController < ApplicationController
 
   def set_item
      @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id		
       redirect_to action: :index
-   end
+    end
   end
 end
