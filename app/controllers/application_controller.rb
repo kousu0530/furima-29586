9f30a17a579fdfb56bbf58893a3eebf3a9da8848
kousu0530 <72140002+kousu0530@users.noreply.github.com>
2021-02-19 17:07:38 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,11 @@ class ApplicationController < ActionController::Base
   
  
 
-  def production?
-    Rails.env.production?
-  end
+ 
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
